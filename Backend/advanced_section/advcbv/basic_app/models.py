33eaae5f18ad439e218feca3eb7a56a6099eb89b
Mioshek <milosz.szczygiel@interia.pl>
2022-11-29 17:14:25 +0100

    Detail View and List View

--- Backend/advanced_section/advcbv/basic_app/models.py
+++ Backend/advanced_section/advcbv/basic_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length=256)
+    principal = models.CharField(max_length=256)
+    location = models.CharField(max_length=256)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+class Student(models.Model):
+    name = models.CharField(max_length=256)
+    age = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    school = models.ForeignKey(School,
+                               related_name='students',
+                               on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.name
--- Backend/advanced_section/advcbv/basic_app/views.py
+++ Backend/advanced_section/advcbv/basic_app/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from django.views.generic import View
-from django.http import HttpResponse
+from django.views.generic import View, TemplateView, ListView, DetailView
+from . import models
+
 
 # Create your views here.
-class CBView(View):
-    def get(self, request):
-        return render(request, 'index.html')
+class IndexView(TemplateView):
+    template_name = 'index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = 'BASIC INJECTION'
+        return context
+    
+
+class SchoolListView(ListView):
+    model = models.School
+    
+class SchoolDetailView(DetailView):
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
